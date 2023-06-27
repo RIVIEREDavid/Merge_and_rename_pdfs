84664f64c2546e13d1c377e588a1e59113effb29
David_R <84936658+RIVIEREDavid@users.noreply.github.com>
2023-06-27 21:06:30 +0200

    Version avec split + boite de dialogue

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import sys
 import shutil
 import tempfile
-from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QLabel, QPushButton, QFileDialog, QListWidget, QWidget, QAbstractItemView
-from PySide6.QtGui import Qt, QDragEnterEvent, QDropEvent, QDragMoveEvent, QMouseEvent, QCursor, QDrag
+from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QLabel, QPushButton, QFileDialog, QListWidget, QWidget, QAbstractItemView, QErrorMessage
+from PySide6.QtGui import Qt
 from PySide6.QtCore import QMimeData
-from PyPDF2 import PdfMerger
+from PyPDF2 import PdfMerger, PdfReader, PdfWriter
 from pathlib import Path
 
 # Classe perso héritant de QListWidget
 class ListBoxWidget(QListWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent)
+    def __init__(self, merge_button, split_button):
+        super().__init__()
         self.setAcceptDrops(True)
         self.resize(600, 600)
         self.setDragDropMode(QAbstractItemView.InternalMove) #type: ignore
+        self.merge_button = merge_button
+        self.split_button = split_button
 
     def dragEnterEvent(self, event):
         if event.mimeData().hasUrls():
@@ -25,25 +27,40 @@ class ListBoxWidget(QListWidget):
         if event.mimeData().hasUrls():
             for url in event.mimeData().urls():
                 file_path = url.toLocalFile()
-                if file_path.endswith(".pdf"):
+                if file_path.endswith(".pdf" or ".PDF"):
                     self.addItem(file_path)
         else:
             super().dropEvent(event)
+
+            # Vérifier le nombre d'articles dans la liste et modifier l'état des boutons en conséquence
+        if self.count() == 1:
+            self.split_button.setEnabled(True)
+            self.merge_button.setEnabled(False)
+        else:
+            self.split_button.setEnabled(False)
+            self.merge_button.setEnabled(True)
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Fusion de fichiers PDF")
+        self.setWindowTitle("Fusion / Split de fichiers PDF")
         self.setMinimumSize(400, 300)
+        self.setWindowFlag(Qt.WindowStaysOnTopHint, True) # type: ignore
 
         layout = QVBoxLayout()
-        label = QLabel("Glissez-déposez vos fichiers PDF ici")
+        label = QLabel("Glissez-déposez les fichiers PDF ici")
         label.setAlignment(Qt.AlignCenter) #type: ignore
         layout.addWidget(label)
 
-        self.list_widget = ListBoxWidget()
+        self.merge_button = QPushButton("Fusionner les fichiers (plusieurs fichiers requis)")
+        self.merge_button.clicked.connect(self.merge_files)
+
+        self.split_button = QPushButton("Splitter le fichier (un seul fichier requis)")
+        self.split_button.clicked.connect(self.split_file)
+
+        self.list_widget = ListBoxWidget(self.merge_button, self.split_button)  #passer en paramètres merge_button et split_button
         self.list_widget.setDragDropMode(QAbstractItemView.InternalMove) #type: ignore
         layout.addWidget(self.list_widget)
 
@@ -51,9 +68,9 @@ class MainWindow(QMainWindow):
         self.button.clicked.connect(self.select_files)
         layout.addWidget(self.button)
 
-        self.merge_button = QPushButton("Fusionner les fichiers")
-        self.merge_button.clicked.connect(self.merge_files)
         layout.addWidget(self.merge_button)
+
+        layout.addWidget(self.split_button)
 
         self.clear_button = QPushButton("Effacer la sélection")
         self.clear_button.clicked.connect(self.clear_list)
@@ -65,16 +82,22 @@ class MainWindow(QMainWindow):
 
         self.file_paths = []
 
+        # Désactiver les boutons split_button et merge_button au démarrage (cad quand la liste est vide)
+        self.split_button.setEnabled(False)
+        self.merge_button.setEnabled(False)
+
+
     def select_files(self):
         file_dialog = QFileDialog()
         file_dialog.setFileMode(QFileDialog.ExistingFiles) #type: ignore
-        file_dialog.setNameFilter("PDF Files (*.pdf)")
+        file_dialog.setNameFilter("PDF Files (*.pdf *.PDF)")
 
         if file_dialog.exec():
             self.file_paths = file_dialog.selectedFiles()
             self.merge_button.setEnabled(True)
             self.list_widget.clear()
             self.list_widget.addItems(self.file_paths)
+
 
     def merge_files(self):
         self.file_paths = [self.list_widget.item(i).text() for i in range(self.list_widget.count())]
@@ -105,9 +128,48 @@ class MainWindow(QMainWindow):
                 for temp_file_path in temp_files:
                     temp_file_path.unlink()
                 shutil.rmtree(temp_dir)
+        
+        self.merge_button.setEnabled(False)
+
 
     def clear_list(self):
         self.list_widget.clear()
+        self.split_button.setEnabled(False)
+        self.merge_button.setEnabled(False)
+
+
+    def split_file(self):
+            file_path = self.list_widget.item(0).text()
+            pdf_reader = PdfReader(file_path)
+            num_pages = len(pdf_reader.pages)
+
+            if num_pages > 1:
+                # Ouvrir une boîte de dialogue pour saisir le nom de fichier racine
+                save_file_box = QFileDialog()
+                save_file_box.setWindowTitle("Nom du fichier racine")
+                save_file_box.setAcceptMode(QFileDialog.AcceptSave) # type: ignore
+                save_file_box.setNameFilter("PDF Files (*.pdf *.PDF)")
+                save_file_box.setDefaultSuffix(".pdf")
+
+                if save_file_box.exec():
+                    root_file_name = save_file_box.selectedFiles()[0]
+                    print(root_file_name)
+
+                    for i, page in enumerate(pdf_reader.pages):
+                        output_file_path = f"{root_file_name[:-4]}_{str(i+1).zfill(2)}.pdf"
+                        page_writer = PdfWriter()
+                        page_writer.add_page(page)
+
+                        with open(output_file_path, "wb") as output_file:
+                            page_writer.write(output_file)
+
+                        self.list_widget.addItem(output_file_path)
+
+                    self.list_widget.takeItem(0)  # Supprimer l'item d'origine
+
+                    self.list_widget.clear()
+                    self.split_button.setEnabled(False)
+                    self.merge_button.setEnabled(False)
 
 
 if __name__ == "__main__":
